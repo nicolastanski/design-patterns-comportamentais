e568d369ab9752f92a86d0388604bb091f03471e
Nicolas Tanski Pavão <nicolas.tanski@gmail.com>
2020-10-11 21:47:08 -0300

    Observer Pattern

--- gera-pedido.php
+++ gera-pedido.php
@@ -1,6 +1,9 @@
 <?php
 
 use Alura\DesignPattern\{GerarPedido, GerarPedidoHandler};
+use Alura\DesignPattern\AcoesAoGerarPedido\CriarPedidoNoBanco;
+use Alura\DesignPattern\AcoesAoGerarPedido\EnviarPedidoPorEmail;
+use Alura\DesignPattern\AcoesAoGerarPedido\LogGerarPedido;
 
 require_once('vendor/autoload.php');
 
@@ -9,5 +12,8 @@ $numeroItens = $argv[2];
 $nomeCliente = $argv[3];
 
 $gerarPedido = new GerarPedido($valorOrcamento, $numeroItens, $nomeCliente);
-$gerarPedidoHandler = new GerarPedidoHandler(/*Dependency Injection*/);
+$gerarPedidoHandler = new GerarPedidoHandler();
+$gerarPedidoHandler->adicionarAcaoAoGerarPedido(new CriarPedidoNoBanco);
+$gerarPedidoHandler->adicionarAcaoAoGerarPedido(new LogGerarPedido);
+$gerarPedidoHandler->adicionarAcaoAoGerarPedido(new EnviarPedidoPorEmail);
 $gerarPedidoHandler->execute($gerarPedido);
